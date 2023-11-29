22cff58197cfeb41f877fef5426fd506eb99044f
Magnus Markling <magnus@markling.com>
2023-11-29 06:41:48 +0100

    add fake error endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use rocket::{get, launch, routes};
+use rocket::{get, http::Status, launch, routes};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, error])
 }
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[get("/-1/error")]
+fn error() -> Status {
+    Status::InternalServerError
 }
