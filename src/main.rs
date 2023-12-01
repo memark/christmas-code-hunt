92fd2b5e210568d841fe66abeabe2a7edc1dfce7
Magnus Markling <magnus@markling.com>
2023-12-01 21:00:54 +0100

    add day 1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use rocket::{get, http::Status, routes};
+use std::{ops::BitXor, path::PathBuf};
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index, error]);
+    let rocket = rocket::build().mount("/", routes![index, error, day_1]);
 
     Ok(rocket.into())
 }
@@ -15,4 +16,14 @@ fn index() -> &'static str {
 #[get("/-1/error")]
 fn error() -> Status {
     Status::InternalServerError
+}
+
+#[get("/1/<nums..>")]
+fn day_1(nums: PathBuf) -> String {
+    nums.iter()
+        .map(|x| x.to_str().unwrap().parse().unwrap())
+        .reduce(i32::bitxor)
+        .unwrap()
+        .pow(3)
+        .to_string()
 }
