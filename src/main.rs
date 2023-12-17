5f3b719645b011c57dcefa07abe4f377b17de6d6
Magnus Markling <magnus@markling.com>
2023-12-17 07:50:07 +0000

    add day 7 task 1 + 2 + 3 (wip)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
+use base64::Engine;
 use fancy_regex::Regex;
-use rocket::{get, http::Status, post, routes, serde::json::Json};
+use rocket::{
+    get,
+    http::{CookieJar, Status},
+    post, routes,
+    serde::json::Json,
+};
 use serde::{Deserialize, Serialize};
-use std::{ops::BitXor, path::PathBuf};
+use std::{collections::HashMap, ops::BitXor, path::PathBuf};
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build().mount(
         "/",
-        routes![index, error, day_1, day_4_strength, day_4_contest, day_6],
+        routes![
+            index,
+            error,
+            day_1,
+            day_4_strength,
+            day_4_contest,
+            day_6,
+            day_7_decode,
+            day_7_bake,
+            day_7_bake2
+        ],
     );
 
     Ok(rocket.into())
@@ -125,4 +141,117 @@ struct ElfCount {
 
     #[serde(rename = "shelf with no elf on it")]
     shelf_with_no_elf_on_it: usize,
+}
+
+#[get("/7/decode")]
+fn day_7_decode(cookies: &CookieJar<'_>) -> String {
+    let base64 = cookies.get("recipe").unwrap().value();
+    let bytes = base64::prelude::BASE64_STANDARD.decode(base64).unwrap();
+    String::from_utf8(bytes).unwrap()
+}
+
+#[get("/7/bake")]
+fn day_7_bake(cookies: &CookieJar<'_>) -> String {
+    let base64 = cookies.get("recipe").unwrap().value();
+    let bytes = base64::prelude::BASE64_STANDARD.decode(base64).unwrap();
+    let string = String::from_utf8(bytes).unwrap();
+    let request = serde_json::from_str::<BakeRequest>(&string).unwrap();
+    println!("{request:#?}");
+
+    let flour = request.pantry.flour / request.recipe.flour;
+    let sugar = request.pantry.sugar / request.recipe.sugar;
+    let butter = request.pantry.butter / request.recipe.butter;
+    let baking_powder = request.pantry.baking_powder / request.recipe.baking_powder;
+    let chocolate_chips = request.pantry.chocolate_chips / request.recipe.chocolate_chips;
+
+    let num_cookies = [flour, sugar, butter, baking_powder, chocolate_chips]
+        .into_iter()
+        .min()
+        .unwrap();
+
+    let response = BakeResponse {
+        cookies: num_cookies,
+        pantry: Ingredients {
+            flour: request.pantry.flour - request.recipe.flour * num_cookies,
+            sugar: request.pantry.sugar - request.recipe.sugar * num_cookies,
+            butter: request.pantry.butter - request.recipe.butter * num_cookies,
+            baking_powder: request.pantry.baking_powder
+                - request.recipe.baking_powder * num_cookies,
+            chocolate_chips: request.pantry.chocolate_chips
+                - request.recipe.chocolate_chips * num_cookies,
+        },
+    };
+    println!("{response:#?}");
+
+    serde_json::to_string_pretty(&response).unwrap()
+}
+
+#[derive(Debug, Deserialize)]
+struct BakeRequest {
+    recipe: Ingredients,
+    pantry: Ingredients,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Ingredients {
+    flour: u64,
+    sugar: u64,
+    butter: u64,
+    #[serde(rename = "baking powder")]
+    baking_powder: u64,
+    #[serde(rename = "chocolate chips")]
+    chocolate_chips: u64,
+}
+
+#[derive(Debug, Serialize)]
+struct BakeResponse {
+    cookies: u64,
+    pantry: Ingredients,
+}
+
+#[get("/7/bake2")]
+fn day_7_bake2(cookies: &CookieJar<'_>) -> String {
+    let base64 = cookies.get("recipe").unwrap().value();
+    let bytes = base64::prelude::BASE64_STANDARD.decode(base64).unwrap();
+    let string = String::from_utf8(bytes).unwrap();
+    let request = serde_json::from_str::<BakeRequest2>(&string).unwrap();
+    println!("{request:#?}");
+
+    // let flour = request.pantry.flour / request.recipe.flour;
+
+    // let num_cookies = [flour, sugar, butter, baking_powder, chocolate_chips]
+    //     .into_iter()
+    //     .min()
+    //     .unwrap();
+
+    let response = BakeResponse2 {
+        cookies: 0,
+        pantry: HashMap::default(),
+    };
+    println!("{response:#?}");
+
+    serde_json::to_string_pretty(&response).unwrap()
+
+    // num_cookies.to_string()
+}
+
+#[derive(Debug, Deserialize)]
+struct BakeRequest2 {
+    recipe: HashMap<String, u64>,
+    pantry: HashMap<String, u64>,
+}
+
+#[derive(Debug, Serialize)]
+struct BakeResponse2 {
+    cookies: u64,
+    pantry: HashMap<String, u64>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: String,
+    username: String,
+
+    #[serde(flatten)]
+    extra: HashMap<String, u64>,
 }
