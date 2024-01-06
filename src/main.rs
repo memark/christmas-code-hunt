a1ed0fdd98bdcf22ae17caa3ba0b39c64b1759df
Magnus Markling <magnus@markling.com>
2024-01-06 14:10:03 +0000

    make module names have two digits

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-mod day_1;
-mod day_4;
-mod day_6;
-mod day_7;
-mod day_8;
-mod day_neg_1;
+mod day_01;
+mod day_04;
+mod day_06;
+mod day_07;
+mod day_08;
+mod day_neg_01;
 mod root;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build()
         .mount("/", root::get_routes())
-        .mount("/-1", day_neg_1::get_routes())
-        .mount("/1", day_1::get_routes())
-        .mount("/4", day_4::get_routes())
-        .mount("/6", day_6::get_routes())
-        .mount("/7", day_7::get_routes())
-        .mount("/8", day_8::get_routes());
+        .mount("/-1", day_neg_01::get_routes())
+        .mount("/1", day_01::get_routes())
+        .mount("/4", day_04::get_routes())
+        .mount("/6", day_06::get_routes())
+        .mount("/7", day_07::get_routes())
+        .mount("/8", day_08::get_routes());
 
     Ok(rocket.into())
 }
