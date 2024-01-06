f6a54614707c92f4e580ec2d582d2da23d5312da
Magnus Markling <magnus@markling.com>
2024-01-06 14:10:31 +0000

    reorder routes

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ mod root;
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build()
-        .mount("/", root::get_routes())
         .mount("/-1", day_neg_01::get_routes())
+        .mount("/", root::get_routes())
         .mount("/1", day_01::get_routes())
         .mount("/4", day_04::get_routes())
         .mount("/6", day_06::get_routes())
